003783803b73927d7fec940f26c62387e3f9be82
Ruslan Hamolia <gamolyaruslan@gmail.com>
2025-05-18 21:44:03 +0300

    Fix token bug

--- internal/database/models/token.go
+++ internal/database/models/token.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Token struct {
@@ -12,4 +13,9 @@ type Token struct {
 	Type           string    `gorm:"not null"` // "confirm", "unsubscribe"
 	SubscriptionID uuid.UUID
 	CreatedAt      time.Time
+}
+
+func (s *Token) BeforeCreate(tx *gorm.DB) error {
+	s.ID = uuid.New()
+	return nil
 }
